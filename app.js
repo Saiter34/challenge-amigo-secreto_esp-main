72f2bee88be5ce17c0c98efc0f9287303a72703c
Lesli Raymundo <lesraymudno3@gmail.com>
2025-02-21 20:01:02 -0500

    Actualiza el style

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos =[];
+function agregarAmigos() {
+    const inputAmigo = document.getElementById ('amigo');
+    const nombreAmigo = inputAmigo.ariaValueMax.trim();
+
+    if (nombre == ""){
+        alert ("Porfavor, inserte un nombre");
+        return;
+    }
+    if (amigos.includes(nombreAmigo)){
+        alertr (`El nombre ${nombreAmigo}` ya esta en la lista);
+        return;
+    }
+    amigos.push(nombreAmigo);
+
+    inputAmigo.ariavalu = "";
+
+    actualizarLista();
+}
